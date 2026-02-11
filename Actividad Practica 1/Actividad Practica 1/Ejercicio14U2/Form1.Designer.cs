6fd69325652789073ab7ded53836b6f4ea47e3fd
raynecmcom-crypto <raynecm.com@gmail.com>
2026-02-11 02:11:08 -0400

    Realizacion del ejercicio 14

---
+++ Actividad Practica 1/Actividad Practica 1/Ejercicio14U2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Ejercicio14U2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            lblInicio = new Label();
+            tabPage2 = new TabPage();
+            lblInfo = new Label();
+            tabPage3 = new TabPage();
+            lblContacto = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(567, 298);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(lblInicio);
+            tabPage1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(559, 270);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Inicio";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // lblInicio
+            // 
+            lblInicio.AutoSize = true;
+            lblInicio.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblInicio.Location = new Point(8, 14);
+            lblInicio.Name = "lblInicio";
+            lblInicio.Size = new Size(214, 17);
+            lblInicio.TabIndex = 0;
+            lblInicio.Text = "Bienvenido a la pantaña de Inicio";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(lblInfo);
+            tabPage2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(366, 247);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Informacion";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblInfo
+            // 
+            lblInfo.AutoSize = true;
+            lblInfo.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblInfo.Location = new Point(8, 17);
+            lblInfo.Name = "lblInfo";
+            lblInfo.Size = new Size(160, 17);
+            lblInfo.TabIndex = 1;
+            lblInfo.Text = "Aqui esta la informacion";
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(lblContacto);
+            tabPage3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(366, 247);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Contacto";
+            tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lblContacto
+            // 
+            lblContacto.AutoSize = true;
+            lblContacto.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblContacto.Location = new Point(8, 13);
+            lblContacto.Name = "lblContacto";
+            lblContacto.Size = new Size(155, 17);
+            lblContacto.TabIndex = 2;
+            lblContacto.Text = "Datos del contacto aqui";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(569, 302);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private Label lblInicio;
+        private Label lblInfo;
+        private Label lblContacto;
+    }
+}
