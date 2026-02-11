305de8f36ca93362c1a56f887f8e31889937524b
raynecmcom-crypto <raynecm.com@gmail.com>
2026-02-11 00:19:26 -0400

    Realizacion del ejercicio 10

---
+++ Actividad Practica 1/Actividad Practica 1/Ejercicio10U2/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Ejercicio10U2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMensaje = new Button();
+            SuspendLayout();
+            // 
+            // btnMensaje
+            // 
+            btnMensaje.BackColor = Color.Black;
+            btnMensaje.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMensaje.ForeColor = Color.White;
+            btnMensaje.Location = new Point(101, 93);
+            btnMensaje.Name = "btnMensaje";
+            btnMensaje.Size = new Size(257, 43);
+            btnMensaje.TabIndex = 0;
+            btnMensaje.Text = "Mostrar Mensaje";
+            btnMensaje.UseVisualStyleBackColor = false;
+            btnMensaje.Click += btnMensaje_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(472, 239);
+            Controls.Add(btnMensaje);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMensaje;
+    }
+}
---
+++ Actividad Practica 1/Actividad Practica 1/Ejercicio10U2/Form1.cs
@@ -0,0 +1,15 @@
+namespace Ejercicio10U2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMensaje_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Soy Raynel Antonio Cordero Mejia y mi Matricula es LR-2024-01548");
+        }
+    }
+}
